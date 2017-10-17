15fd8481f490ab22e962a3436cc389291cc2fd3d
elijahprince73 <elijahprince73@gmail.com>
2017-10-17 10:57:53 -0700

    logged youtube results

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAtlLv-kCX-7exW0eNOMGetHxjIin1rqIM'
+
+YTSearch({
+  key: API_KEY,
+  term: 'serfboards'
+}, function(data) {
+  console.log(data);
+})
 
 const App = () => {
   return (
