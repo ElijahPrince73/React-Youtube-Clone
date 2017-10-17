294959c470731b05b61c587fc8c8a415a0e9036d
elijahprince73 <elijahprince73@gmail.com>
2017-10-17 14:59:11 -0700

    Handling Null props

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import React, {
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
-import VideoList from './components/video_list.js'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyAtlLv-kCX-7exW0eNOMGetHxjIin1rqIM'
 
@@ -30,10 +31,11 @@ class App extends Component {
     return (
       <div>
       <SearchBar/>
+      <VideoDetail video={this.state.videos[0]}/>
       <VideoList videos={this.state.videos}/>
     </div>
-    )
-  }
-}
+    );
+  };
+};
 
 ReactDOM.render(<App/>, document.getElementById('container'))
